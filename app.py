b871d10aff7296a5f56a3a27111f5606dc31d749
siddheshshaji <62932933+siddheshshaji@users.noreply.github.com>
2020-06-07 03:49:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# IMPORTING THE NECESSARY LIBRARIES
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
@@ -6,26 +7,32 @@ import logging
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
+# INITIALIZING SENTRY(FOR LOGGING)
 sentry_sdk.init(
     dsn="https://52c6e8b6f80c4a2f9ced545c629958c9@o401877.ingest.sentry.io/5262168",
     integrations=[FlaskIntegration()]
 )
 
+# INITIALIZING THE APP
 app = Flask(__name__)
+
+# IMPORTING THE MODEL
 model = pickle.load(open('cardio_2.pkl', 'rb'))
 
+# LANDING PAGE
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# 404 PAGE HANDLER
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
 
+# PREDICTION ROUTE
 @app.route('/predict',methods=['POST'])
-
 def predict():
-    features = list([float(x) for x in request.form.values()])
+    features = list([float(x) for x in request.form.values()]) # THE MODEL NEEDS AN INPUT ARRAY 
     if model.predict([features])==[1]:
         return render_template('home.html', prediction_text="You most probably have a cardiovascular disease... Please get yourself checked by a physician.")
     else:
